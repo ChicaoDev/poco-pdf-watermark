c391cc38f14cf9e48e6e17d499d057b4f735499e
Francisco Costa <francisco.costa@evolutionit.com.br>
2017-08-17 11:03:49 -0300

    initial commit

---
+++ src/POCO.Pdf.Watermark/POCO.Pdf.Watermark/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace POCO.Pdf.Watermark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo pdfFileInfo = new FileInfo("c:\\temp\\input.pdf");
+
+            byte[] fileBytes = WatermarkPdfHelper.WriteToPdf(pdfFileInfo
+                , "This document was approved by Francisco Costa on " + DateTime.Now.ToString("dddd, MMMM d, yyyy", CultureInfo.GetCultureInfo("en-US")));
+
+            File.WriteAllBytes("c:\\temp\\output.pdf", fileBytes);
+        }
+    }
+}
---
+++ src/POCO.Pdf.Watermark/POCO.Pdf.Watermark/WatermarkPdfHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace POCO.Pdf.Watermark
+{
+    public class WatermarkPdfHelper
+    {
+        public static byte[] WriteToPdf(FileInfo sourceFile, string watermarkText)
+        {
+            PdfReader reader = new PdfReader(sourceFile.FullName);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                PdfStamper pdfStamper = new PdfStamper(reader, memoryStream);
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    Rectangle pageSize = reader.GetPageSizeWithRotation(i);
+
+                    PdfContentByte pdfPageContents = pdfStamper.GetUnderContent(i);
+                    pdfPageContents.BeginText();
+
+                    BaseFont baseFont = BaseFont.CreateFont(BaseFont.COURIER_BOLD, Encoding.ASCII.EncodingName, false);
+                    pdfPageContents.SetFontAndSize(baseFont, 10); 
+                    pdfPageContents.SetRGBColorFill(255, 0, 0); 
+
+                    float textAngle = 90.0f;
+
+                    pdfPageContents.ShowTextAligned(PdfContentByte.ALIGN_CENTER, watermarkText, 25, pageSize.Width / 2, textAngle);
+
+                    pdfPageContents.EndText();
+                }
+
+                pdfStamper.FormFlattening = true; 
+                pdfStamper.Close(); 
+
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
